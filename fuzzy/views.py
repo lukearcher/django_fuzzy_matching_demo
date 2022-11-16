45ea86424ffb351829218c3f436a2074eeac39b2
lukearcher <lukearcher1@gmail.com>
2022-11-16 16:25:46 +1100

    first version

--- fuzzy/views.py
+++ fuzzy/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'fuzzy/home.html', {'message': ''})
+
+def fuzzy(request):
+
+    #sentiment_phrase = request.GET.get('phrase', '')
+
+    return_message = 'Fuzzy Matching Results: '
+
+    return render(request, 'fuzzy/home.html', {'message': return_message})
